ed16faf98d5cfe6842cc5ad24dc23ff4393973d4
liorcaspi9 <liorcaspi9@gmail.com>
2022-01-23 01:33:58 +0200

    consts

--- src/features/safe/safeConsts.js
+++ src/features/safe/safeConsts.js
@@ -5,3 +5,10 @@ export const BUTTONS_SOUND_PATH = "sounds/beep-07a.wav";
 export const ERROR_SOUND_PATH = "sounds/beep-03.mp3";
 export const UNLOCK_SOUND_PATH = "sounds/rotary-phone-2-nr0.mp3";
 export const OPEN_SOUND_PATH = "sounds/open.mp3";
+export const ERROR_TEXT = 'err1';
+export const SOUNDS = {
+    ERROR_SOUND: new Audio(ERROR_SOUND_PATH),
+    UNLOCK_SOUND: new Audio(UNLOCK_SOUND_PATH),
+    OPEN_SOUND: new Audio(OPEN_SOUND_PATH),
+    BUTTONS_SOUND: new Audio(BUTTONS_SOUND_PATH)
+}
